59b2870ff6a26e1840fa5fff053561d3855b3087
joabehans <joabehans@gmail.com>
2026-08-23 20:52:29 -0300

    Fix CSP blocking OneSignal Web SDK script/worker/connect

---
+++ public/onesignal-init.js
@@ -0,0 +1,6 @@
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+OneSignalDeferred.push(async function (OneSignal) {
+  await OneSignal.init({
+    appId: "264b854b-0c7d-4180-b6fd-36f9b2469847",
+  });
+});
